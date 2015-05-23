9f83682d0aa3d6387507b3c5eaad20101a1e0fa8
Nikolay Bienko <bikolya@gmail.com>
2015-05-23 07:46:09 +0300

    Add WebSocket API to interact with server

---
+++ public_html/js/api/socket.js
@@ -0,0 +1,40 @@
+define([
+  'app'
+], function(app) {
+
+  var ws = {
+    url: "ws://" + location.host + '/gameplay',
+    closed: true,
+
+    connect: function() {
+
+      socket = new WebSocket(this.url);
+
+      socket.onopen = function(data) {
+        this.closed = false;
+        app.wsEventBus.trigger('ws:' + data.type, '/game');
+      };
+
+      socket.onclose = function(event) {
+        this.closed = true;
+      };
+
+      socket.onmessage = function(message) {
+        data = JSON.parse(message.data);
+        app.wsEventBus.trigger('ws:' + data.type, data.body);
+      };
+
+      this.socket = socket;
+    },
+
+    send: function(msg) {
+      this.socket.send(JSON.stringify(msg));
+    },
+
+    close: function(msg) {
+      this.socket.close();
+    }
+  };
+
+  return ws;
+});
